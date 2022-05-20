6cc8ff2b536f59afbcd1708200e778ab744b1fc3
devmemory <devmemorydh@gmail.com>
2022-05-20 17:57:19 +0900

    - test

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import './app.css'
 
 function App() {
+    const [data, setData] = useState(2)
+
+    console.log({data})
+
+    useEffect(() => {
+        setTimeout(() => {
+            setData(4)
+        }, 2000);
+    }, [])
     return (
-        <div>React without CRA</div>
+        <div className='div_main'>React without CRA {data}</div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,8 +4,4 @@ import App from './app'
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<App />)
